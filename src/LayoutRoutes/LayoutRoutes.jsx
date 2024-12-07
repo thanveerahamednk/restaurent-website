6cbcf5b4e967e1b8059862e5cfcfc2e08d1d3a47
thanveerahamednk <thanveerahmednk@gmail.com>
2024-12-07 16:48:17 +0530

    git deploy changes

--- src/LayoutRoutes/LayoutRoutes.jsx
+++ src/LayoutRoutes/LayoutRoutes.jsx
@@ -15,6 +15,7 @@ function LayoutRoutes() {
 <Header/>
 <Routes>
 <Route path='/' element={<Home/>} />
+<Route path='/restaurent-website' element={<Home/>} />
 <Route path='/dishes' element={<Dishes/>} />
 <Route path='/services' element={<Booking/>} />
 <Route path='/about' element={<About/>} />
